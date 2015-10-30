cd4ad5fc8c551ebc7ee26d8565f3e9e1f99ddcbc
Fincher Justin <zhtsu47@me.com>
2015-10-30 14:58:39 +0800

    First commit

---
+++ PassbusterTests/PassbusterTests.m
@@ -0,0 +1,39 @@
+//
+//  PassbusterTests.m
+//  PassbusterTests
+//
+//  Created by Fincher Justin on 15/10/29.
+//  Copyright © 2015年 Fincher Justin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PassbusterTests : XCTestCase
+
+@end
+
+@implementation PassbusterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'Passbuster' do
+
+pod 'IQKeyboardManager'
+pod 'KVNProgress'
+pod 'MJExtension'
+pod 'FMDB'
+pod "JazzHands", "<= 2.0.2"
+pod 'CocoaSecurity'
+
+end
+
+target 'PassbusterTests' do
+
+end
+
+target 'PassbusterUITests' do
+
+end
+
